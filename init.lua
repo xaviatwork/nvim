09013bdd5e38521503d209eb8e915930e337a850
Xavi Aznar <xavier.aznar@seat.es>
2024-01-01 07:20:44 +0000

    extract catppuccin config to its own file

--- init.lua
+++ init.lua
@@ -23,11 +23,6 @@ local opts = {}
 
 require("lazy").setup("plugins")
 
-require("catppuccin").setup({
-  flavour = "mocha"
-})
-vim.cmd.colorscheme "catppuccin"
-
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
